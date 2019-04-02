bf16aa439282210a7738f957ebe36bf8194c9130
Hugo Adams <hugoadams1412@gmail.com>
2019-04-02 23:19:39 +0000

    Actually got it working, not the way I would like though

--- test.c
+++ test.c
@@ -2,14 +2,15 @@
 
 int main() {
 
-float[500] x;
-int i, store = 0;
+int x[] = {13, 214, 982, 24982, 124, 412944, 24, 244, 547, 356};
+int i = 0, store = 0;
 
-for(i = 0; i < 500; i++) {
-if(x[i] > store)
+for(i = 0; i < 10; i++) {
+if(x[i] > store) {
 store = x[i];
-i++;
-}
+    }
+ }
+
 printf("The largest element is: %d\n", store);
 
 return 0;
